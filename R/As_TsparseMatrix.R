65d14243cbae98efd8ed189dafb1f746814d4b44
Langsrud <oyl@ssb.no>
2022-08-15 14:38:53 +0200

    Check Matrix version when matrix class. Since code fails in earlier versions of Matrix.

--- R/As_TsparseMatrix.R
+++ R/As_TsparseMatrix.R
@@ -10,6 +10,7 @@
 #' @param do_drop0  whether to run `drop0`
 #'
 #' @return A matrix. Virtual class is `TsparseMatrix`. Class `dgTMatrix` expected.
+#' @importFrom utils compareVersion packageVersion
 #' @export
 #' 
 #' @note `Matrix:::.as.via.virtual` in development version of package `Matrix` (date 2022-08-13) used to generate code.
@@ -28,7 +29,10 @@ As_TsparseMatrix <- function(from, do_drop0 = TRUE) {
     return(as(from, "TsparseMatrix"))  # Matrix:::.as.via.virtual('dgCMatrix', 'dgTMatrix'); Matrix:::.as.via.virtual('dgeMatrix', 'dgTMatrix')
   }
   
-  if (inherits(from, "matrix")) {
+  if (inherits(from, "matrix")) {  # But "matrix" is not class after drop0
+    if (compareVersion(as.character(packageVersion("Matrix")), "1.4.2") < 0) {   # Since code fails in earlier versions of Matrix.
+      return(as(from, "dgTMatrix"))
+    }
     return(as(as(as(from, "dMatrix"), "generalMatrix"), "TsparseMatrix"))  # Matrix:::.as.via.virtual('matrix', 'dgTMatrix')
   }
   
