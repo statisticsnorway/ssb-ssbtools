87cb440b15d3a971ab3be13bd36b933654a09dbe
Langsrud <oyl@ssb.no>
2022-08-16 14:54:15 +0200

    Remove Temporary onload for checking

--- R/onLoad.R
+++
@@ -1,8 +0,0 @@
-
-.onLoad <- function(libname, pkgname){
-  options(Matrix.warnDeprecatedCoerce = 2) 
-}
-
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste('Development version of', pkgname, 'with options(Matrix.warnDeprecatedCoerce = 2) \n'))
-}
