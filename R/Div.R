263f0aa4aecf198416c196b60633cf5ae69a9cd2
Langsrud <oyl@ssb.no>
2023-03-20 14:06:55 +0100

    Reuse own function from other package to produce warning text

--- R/Div.R
+++ R/Div.R
@@ -159,11 +159,13 @@ CrossAll <- function(data, hierarchies = TRUE) {
 }
 
 
-
-
-
-
-
-
+# Kostra:::HeadEnd
+# HeadEnd(1:1000) '1' '2' '3' '4' '...'  '1000'
+HeadEnd <- function(x, n = 4L) {
+  x <- as.character(x)
+  if (length(x) > (n + 2))
+    x <- c(head(x, n = n), "...", tail(x, n = 1))
+  x
+}
 
 
