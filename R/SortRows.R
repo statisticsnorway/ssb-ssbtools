89b6b02b43d4e36f0d5e6af6ada6a2a4374a1a5c
Langsrud <oyl@ssb.no>
2023-05-26 15:21:50 +0200

    SortRows now better documented Not longer marked with "keywords internal" Since this function can be very useful

--- R/SortRows.R
+++ R/SortRows.R
@@ -1,16 +1,21 @@
 
-#' Sorting rows
+#' Sorting rows of a matrix or data frame
 #'
-#' @param m 
-#' @param cols 
-#' @param index.return 
+#' @param m matrix or data frame
+#' @param cols Indexes of columns, in the desired order, used for sorting.
+#' @param index.return logical indicating if the ordering index vector should be 
+#'                     returned instead of sorted input. 
 #'
-#' @return sorted m
+#' @return sorted `m` or a row index vector
 #' @export
 #' @author Øyvind Langsrud
-#' @keywords internal
 #'
 #' @examples
+#' d <- SSBtoolsData("d2w")
+#' SortRows(d[4:7])
+#' SortRows(d, cols = 4:7)
+#' SortRows(d, cols = c(2, 4))
+#' 
 #' SortRows(matrix(sample(1:3,15,TRUE),5,3))
 SortRows <- function(m, cols = 1:dim(m)[2], index.return = FALSE) {
   ix <- eval(parse(text = paste("order(", paste("m[,", cols, ",drop=TRUE]", sep = "", collapse = ","), 
