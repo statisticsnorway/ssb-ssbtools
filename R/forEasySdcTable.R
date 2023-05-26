5b0a04d8dda613ce9617dc8169421c74497249e8
Langsrud <oyl@ssb.no>
2023-05-26 15:02:34 +0200

    SortRows moved to a separate file

--- R/forEasySdcTable.R
+++ R/forEasySdcTable.R
@@ -1,28 +1,4 @@
 
-
-
-#' Sorting rows
-#'
-#' @param m 
-#' @param cols 
-#' @param index.return 
-#'
-#' @return sorted m
-#' @export
-#' @author Øyvind Langsrud
-#' @keywords internal
-#'
-#' @examples
-#' SortRows(matrix(sample(1:3,15,TRUE),5,3))
-SortRows <- function(m, cols = 1:dim(m)[2], index.return = FALSE) {
-  ix <- eval(parse(text = paste("order(", paste("m[,", cols, ",drop=TRUE]", sep = "", collapse = ","), 
-                                ")")))
-  if (index.return) 
-    return(ix)
-  m[ix, , drop = FALSE]
-}
-
-
 #' Finding table(s) of hierarchical variable groups
 #'
 #' A single table or two linked tables are found
