12816edba8a6b8d8117993dec4c293440a2b6bb3
Langsrud <oyl@ssb.no>
2023-05-27 11:09:34 +0200

    UniqueSeq moved to a separate file.

--- R/Div.R
+++ R/Div.R
@@ -1,25 +1,3 @@
-#' Sequence within unique values 
-#'
-#' @param x vector 
-#' @param sortdata matrix or vector to determine sequence order
-#'
-#' @return integer vector
-#' @export
-#' @author Øyvind Langsrud
-#'
-#' @examples
-#' # 1:4 within A and 1:2 within B
-#' UniqueSeq(c("A", "A", "B", "B", "A", "A"))
-#' 
-#' # Ordered differently
-#' UniqueSeq(c("A", "A", "B", "B", "A", "A"), c(4, 5, 20, 10, 3, 0))
-UniqueSeq <- function(x, sortdata = matrix(1L, length(x), 0)) {
-  ix <- SortRows(cbind(x, sortdata), index.return = TRUE)
-  nr <- seq_len(length(x))
-  sortx <- x[ix]
-  (1L + nr - nr[match(sortx, sortx)])[order(ix)]
-}
-
 
 
 #' Round values that are close two whole numbers
