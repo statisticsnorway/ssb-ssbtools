fabc3846da7ec4ae7e4b21187bdea23f78639343
Langsrud <oyl@ssb.no>
2023-09-12 12:27:43 +0200

    change of indent

--- R/GaussSuppression.R
+++ R/GaussSuppression.R
@@ -972,36 +972,36 @@ GaussSuppression1 <- function(x, candidates, primary, printInc, singleton, nForc
       n_unique <- length(unique(singleton_num[as.logical(singleton_num)]))
       singleP <- unique(singleP)
       if (!forceSingleton2Primary) {
-      if (length(rowsP)) {
-        WhenEliminatedRowsSingleton(paste(length(rowsP), "out of", sum(as.logical(singleton_num)), "singleton rows eliminated.", length(singleP), "out of", n_unique, "unique singletons problematic."))
-      } else {
-        if (length(singleP)) {
-          WhenEliminatedRowsSingleton(paste(length(singleP), "out of", n_unique, "unique singletons problematic although no rows eliminated in both parallels."))
-        }
-      }
-      }
-      if (forceSingleton2Primary) {
-      if (length(singleP)) {
-        eliminatedBySingleton <- rep(FALSE, length(singleP))
-        for (i in seq_len(n_relevant_primary)) {
-          if (!length(B$r[[i]])) {     # Avoid special situation
-            primarySingletonNum[i] <- 0
+        if (length(rowsP)) {
+          WhenEliminatedRowsSingleton(paste(length(rowsP), "out of", sum(as.logical(singleton_num)), "singleton rows eliminated.", length(singleP), "out of", n_unique, "unique singletons problematic."))
+        } else {
+          if (length(singleP)) {
+            WhenEliminatedRowsSingleton(paste(length(singleP), "out of", n_unique, "unique singletons problematic although no rows eliminated in both parallels."))
           }
         }
-        B$r <- B$r[seq_len(n_relevant_primary)]
-        B$x <- B$x[seq_len(n_relevant_primary)]
-        primarySingletonNum <- primarySingletonNum[seq_len(n_relevant_primary)]
-        kk_2_factorsB <- kk_2_factorsB[seq_len(n_relevant_primary)]
-        for (i in seq_along(singleP)) {
-          p <- primarySingletonNum == singleP[i]
-          eliminatedBySingleton[i] <- AnyEliminatedBySingleton(list(r = B$r[p], x = B$x[p]), 
-                                                               list(r = B$r[!p], x = B$x[!p]), 
-                                                               kk_2_factorsB[p], kk_2_factorsB[!p], 
-                                                               singleton = singleton,
-                                                               DoTestMaxInt = DoTestMaxInt, tolGauss = tolGauss)
-        }
-        WhenEliminatedRowsSingleton(paste(sum(eliminatedBySingleton), "out of", n_unique, "unique singletons can reveal primary cells."))
       }
+      if (forceSingleton2Primary) {
+        if (length(singleP)) {
+          eliminatedBySingleton <- rep(FALSE, length(singleP))
+          for (i in seq_len(n_relevant_primary)) {
+            if (!length(B$r[[i]])) {     # Avoid special situation
+              primarySingletonNum[i] <- 0
+            }
+          }
+          B$r <- B$r[seq_len(n_relevant_primary)]
+          B$x <- B$x[seq_len(n_relevant_primary)]
+          primarySingletonNum <- primarySingletonNum[seq_len(n_relevant_primary)]
+          kk_2_factorsB <- kk_2_factorsB[seq_len(n_relevant_primary)]
+          for (i in seq_along(singleP)) {
+            p <- primarySingletonNum == singleP[i]
+            eliminatedBySingleton[i] <- AnyEliminatedBySingleton(list(r = B$r[p], x = B$x[p]), 
+                                                                 list(r = B$r[!p], x = B$x[!p]), 
+                                                                 kk_2_factorsB[p], kk_2_factorsB[!p], 
+                                                                 singleton = singleton,
+                                                                 DoTestMaxInt = DoTestMaxInt, tolGauss = tolGauss)
+          }
+          WhenEliminatedRowsSingleton(paste(sum(eliminatedBySingleton), "out of", n_unique, "unique singletons can reveal primary cells."))
+        }
       }  
     }
     NULL
