1e41a26d51b3d9352c65c92079c0cb358eca25dd
Langsrud <oyl@ssb.no>
2023-10-10 13:48:04 +0200

    Copy of function. Will be changed in next commit.
    Function in the GaussSuppression main file.
    Will be changed to FindParentChild

--- R/anySumNOTprimary.R
+++ R/anySumNOTprimary.R
@@ -21,3 +21,21 @@ Any0GaussInt <- function(r, rB) {
   }
   FALSE
 }
+
+
+
+
+FindDiffMatrix <- function(x, y = x, max_colSums_diff = Inf) {
+  xty <- As_TsparseMatrix(crossprod(x, y))
+  colSums_y_xty_j_1 <- colSums(y)[xty@j + 1]
+  # finds children in x and parents in y
+  r <- colSums(x)[xty@i + 1] == xty@x & 
+    colSums_y_xty_j_1     != xty@x & 
+    (colSums_y_xty_j_1 - xty@x) <= max_colSums_diff
+  child <- xty@i[r] + 1L
+  parent <- xty@j[r] + 1L
+  diff_matrix <- y[, parent, drop = FALSE] - 
+    x[, child, drop = FALSE]
+  colnames(diff_matrix) <- parent
+  diff_matrix
+}
