de7cc9199e020c57c013f07f03a9dfe7d757a7c5
Langsrud <oyl@ssb.no>
2025-01-28 14:34:42 +0100

    tables_by_formulas with documentation and extra parameters
    auto_collapse and hierarchical_extend0  are new parameres in this commit
    
    Documentation refinement with the help of ChatGPT

--- R/tables_by_formulas.R
+++ R/tables_by_formulas.R
@@ -1,16 +1,40 @@
 
-
-#' tables_by_formulas
+#' Tabular Statistics Based on Formulas
 #'
-#' @param data data 
-#' @param ...  dots
-#' @param table_fun table_fun 
-#' @param table_formulas table_formulas 
-#' @param substitute_vars substitute_vars 
-#' @param collapse_vars collapse_vars 
-#' @param total total
+#' This function acts as an overlay for functions that produce table statistics 
+#' through an interface utilizing the [ModelMatrix()] function and its `formula` parameter. 
+#' Each table (individual statistic) is defined by a formula. The output is a single `data.frame` 
+#' that contains the results for all tables.
+#' 
+#' To ensure full control over the generated output variables, `table_fun` is called with `avoid_hierarchical` 
+#' or `avoidHierarchical` set to `TRUE`. Desired variables in the output are achieved using 
+#' `substitute_vars`, `auto_collapse`, and `collapse_vars`.
+#' 
+#' If `table_fun` automatically uses [Extend0()], the parameter `hierarchical_extend0` 
+#' specifies the `hierarchical` parameter in [Extend0()] via [Extend0fromModelMatrixInput()]. 
+#' When `hierarchical_extend0` is `TRUE`, hierarchies are generated automatically.
+#' By default, it is set to `TRUE`, preventing excessive data extension and aligning with 
+#' the default behavior of [Formula2ModelMatrix()], where `avoidHierarchical = FALSE`.
+#' 
+#' Note: The use of `total_collapse` internally allows handling of variable names not present in the data. 
+#' This ensures flexibility when modifying the `table_formulas` parameter.
 #'
-#' @return A data frame 
+#' @param data The input data to be processed by `table_fun`.
+#' @param table_fun The table-producing function to be used.
+#' @param ...  Additional arguments passed to `table_fun`.
+#' @param table_formulas A named list of formulas, where each entry defines a specific table.  
+#' @param substitute_vars Allows formulas in `table_formulas` to be written in a simplified way. 
+#'                        If `substitute_vars` is specified, the final formulas are generated 
+#'                        using [substitute_formula_vars()] with `substitute_vars` as input.
+#' @param auto_collapse Logical. If `TRUE`, variables are collapsed using [total_collapse()] 
+#'                      with the `variables` parameter according to `substitute_vars`.             
+#' @param collapse_vars When specified, [total_collapse()] is called with `collapse_vars` as the `variables` parameter, 
+#'                      after any call triggered by the `auto_collapse` parameter.
+#' @param total A string used to name totals. Passed to both `table_fun` and [total_collapse()].  
+#' @param hierarchical_extend0 Controls automatic hierarchy generation for [Extend0()]. 
+#'                              See "Details" for more information. 
+#'
+#' @return A single `data.frame` containing results for all tables defined in `table_formulas`.
 #' @export
 #' 
 #' @seealso \code{\link{filter_by_variable}}
@@ -25,13 +49,15 @@
 #'                    collapse_vars = list(sector = c("sector2", "sector4")), 
 #'                    sum_vars = "value", 
 #'                    total = "T")
-tables_by_formulas <- function(data, 
-                               ..., 
+tables_by_formulas <- function(data,
                                table_fun, 
+                               ..., 
                                table_formulas, 
-                               substitute_vars = NULL, 
+                               substitute_vars = NULL,
+                               auto_collapse = TRUE,
                                collapse_vars = NULL, 
-                               total = "Total") {
+                               total = "Total",
+                               hierarchical_extend0 = TRUE) {
   
   if (length(substitute_vars)) {
     for (i in seq_along(table_formulas)) {
@@ -42,7 +68,11 @@ tables_by_formulas <- function(data,
   
   formula <- combine_formulas(table_formulas)
   
-  a <- table_fun(data, ..., formula = formula, avoid_hierarchical = TRUE, avoidHierarchical = TRUE, total = total)
+  a <- table_fun(data, ..., 
+                 formula = formula, 
+                 avoid_hierarchical = TRUE, avoidHierarchical = TRUE, 
+                 total = total, 
+                 hierarchical_extend0 = hierarchical_extend0)
   
   startRow <- attr(a, "startRow", exact = TRUE)
   
@@ -53,7 +83,7 @@ tables_by_formulas <- function(data,
     table_indicators[[i]] <- formula_selection(a, table_formulas[[i]], logical = TRUE)
   }
   
-  if (length(substitute_vars)) {
+  if (auto_collapse & length(substitute_vars)) {
     a <- total_collapse_allow_missing(a, substitute_vars_removed, total = total) 
   }
   
